e95d7fa9d18f5e145b9a92e9d6bba7260dcd1cc5
Taiwo Akinosho <tiwiex@yahoo.com>
2018-11-22 02:32:13 +0100

    recipe list completed with items

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from '../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A recipe', 'my first mean is cool', 'https://savorysweetlife.com/wp-content/uploads/2010/03/chicken-marsala-1.jpg' ),
+    new Recipe('A recipe', 'my first mean is cool', 'https://savorysweetlife.com/wp-content/uploads/2010/03/chicken-marsala-1.jpg' )
+  ];
 
   constructor() { }
 
